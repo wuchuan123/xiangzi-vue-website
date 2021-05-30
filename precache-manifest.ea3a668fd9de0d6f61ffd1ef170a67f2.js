4d003294a66a1ac35e7c161faca1569ce15fd516
吴川 <wuchuan@wuchuandeMacBook-Pro.local>
2021-05-30 16:25:18 +0800

    deploy

---
+++ precache-manifest.ea3a668fd9de0d6f61ffd1ef170a67f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78583c807bd59552ed72",
+    "url": "/xiangzi-vue-website/css/app.9c4ac18a.css"
+  },
+  {
+    "revision": "7aa5aa65951ba9f758277973856efdf4",
+    "url": "/xiangzi-vue-website/index.html"
+  },
+  {
+    "revision": "78583c807bd59552ed72",
+    "url": "/xiangzi-vue-website/js/app.17634752.js"
+  },
+  {
+    "revision": "138cdc38b888f03218dd",
+    "url": "/xiangzi-vue-website/js/chunk-vendors.1a43ba33.js"
+  },
+  {
+    "revision": "e60892bb427f1745766bad0d807f6071",
+    "url": "/xiangzi-vue-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/xiangzi-vue-website/robots.txt"
+  }
+]);
